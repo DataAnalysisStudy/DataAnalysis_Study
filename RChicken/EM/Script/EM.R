c48b8cb1826b23a72750c9ff06f4b4b9aff4f284
kiminch3000 <kiminch3000@naver.com>
2018-07-13 08:04:32 +0900

    Update EM.R

--- RChicken/EM/Script/EM.R
+++ RChicken/EM/Script/EM.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 library(plotly)
 
-####### ?곗씠????援ъ텞
+####### 데이터 셋 구축
 
 x1 <- rnorm(50, 10, 2)
 x2 <- rnorm(50, 20, 1.5)
@@ -12,23 +12,48 @@ y3 <- rnorm(50, 30, 2)
 
 x <- c(x1, x2)
 y <- c(y1, y2)
-
-x <- c(x1, x2, x3)
-y <- c(y1, y2, y3)
+# 
+# x <- c(x1, x2, x3)
+# y <- c(y1, y2, y3)
 
 data <- data.frame(x = x, y = y)
 
-plot_ly(dat, x = ~x, y = ~y)
+
+
+####### 원 중심과 sigma 1, 2 등고선 그리기 
+
+graph <- function(){
+  plot_ly(data, x = ~x, y = ~y, color = ~class, type = "scatter") %>%
+    add_trace(x = ~x, y = ~y, color = ~class, data = parameter, type = "scatter") %>%
+    layout(shapes = list(
+      list(xref = 'x', yref = 'y', 
+           x0 = parameter$x[1] - parameter$sigma[1], 
+           x1 = parameter$x[1] + parameter$sigma[1], 
+           y0 = parameter$y[1] - parameter$sigma[1],
+           y1 = parameter$y[1] + parameter$sigma[1], 
+           type = "circle", opacity = 0.2,
+           fillcolor = 'rgb(50, 20, 90)', line = list(color = 'rgb(50, 20, 90)')), 
+      list(xref = 'x', yref = 'y', 
+           x0 = parameter$x[2] - parameter$sigma[2], 
+           x1 = parameter$x[2] + parameter$sigma[2], 
+           y0 = parameter$y[2] - parameter$sigma[2],
+           y1 = parameter$y[2] + parameter$sigma[2], 
+           type = "circle", opacity = 0.2,
+           fillcolor = 'rgb(90, 200, 75)', line = list(color = 'rgb(90, 200, 75)'))
+    )
+    )
+}
+
 
 
 ####### EM
 
 dimension = 2
 
-################# ?뺢퇋遺꾪룷 珥덇린 ?뚮씪誘명꽣 ?ㅼ젙
+################# 정규분포 초기 파라미터 설정
 parameter <- data.frame()
 
-#### mu??媛곴컖??李⑥썝???ш린 ?쒖쑝濡??뺣젹???ㅼ쓬, 李⑥썝 ?섎쭔??遺꾩쐞?섎줈 ?섎닎
+#### mu는 각각의 차원을 크기 순으로 정렬한 다음, 차원 수만큼 분위수로 나눔
 mu <- apply(data, 2, function(x){
   quantile(x, seq(0, 1, length.out = 2 + dimension))
   
@@ -36,26 +61,27 @@ mu <- apply(data, 2, function(x){
 )
 mu <- mu[-c(1, nrow(mu)), ]
 
-#### sigma??媛곴컖??李⑥썝??0.841遺꾩쐞?쒖뿉??0.5遺꾩쐞??1sigma)瑜?類 媛?
+#### sigma는 각각의 차원의 0.841분위서에서 0.5분위수(1sigma)를 뺀 값 
 sigma <- apply(data, 2, function(x){
   diff(quantile(x, c(0.5, 0.841)))
 }
 )
 
-#### 媛곴컖??李⑥썝??異붿텧???뺣쪧? 洹좊벑遺꾪룷濡??ㅼ젙
+#### 각각의 차원에 추출될 확률은 균등분포로 설정
 tou <- rep(1/dimension, dimension)
 
-#### ?뚮씪誘명꽣 ?곗씠???꾨젅???앹꽦 
+#### 파라미터 데이터 프레임 생성 
 
 parameter <- data.frame()
 parameter <- rbind(parameter, mu)
 parameter <- cbind(parameter, sigma)
 parameter <- cbind(parameter, tou)
+parameter <- cbind(parameter, class = paste("center", factor(1:dimension), sep = "_"))
 
 
 ################# Expectation Step
 
-#### 珥덇린 ?뚮씪誘명꽣 ?ㅼ젙?쒕?濡??곗씠??class 吏??
+#### 초기 파라미터 설정한대로 데이터 class 지정 
 for(i in 1:dimension){
   
   if(i == 1){
@@ -65,14 +91,14 @@ for(i in 1:dimension){
     
     fx <- data.frame(apply(data, 1, function(x){
       x_mu <- as.numeric(x - parameter[i, 1:dimension])
-      exp(-1/2 * t(x_mu) %*% covInv %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp)) ## i ?뺢퇋遺꾪룷???랁븷 ?뺣쪧
+      exp(-1/2 * t(x_mu) %*% covInv %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp)) ## i 정규분포에 속할 확률
     }))
     
   }else{
     
     fx <- cbind(fx, apply(data, 1, function(x){
       x_mu <- as.numeric(x - parameter[i, 1:dimension])
-      exp(-1/2 * t(x_mu) %*% covInv %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp)) ## i ?뺢퇋遺꾪룷???랁븷 ?뺣쪧
+      exp(-1/2 * t(x_mu) %*% covInv %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp)) ## i 정규분포에 속할 확률
     }))
     
   }
@@ -88,39 +114,10 @@ for(i in 1:dimension){
   
 }
 
-data$class <- as.factor(apply(fx, 1, which.max))
+data$class <- as.factor(apply(fx, 1, which.max)) ## 초기값을 이용하여 likelihood가 가장 높은 클러스터에 배정
 
-plot_ly(data, x = ~x, y = ~y, color = ~class)
-parameter
-fx
-
-log(parameter$pi[i]) - 
-  1/2 * log(abs(det(covTemp))) - 
-  1/2 * x_mu %*% covInv %*% t(x_mu)
-
-e <- x_mu %*% covInv
-e <- e[1:10, ]
-
-e
-
-
-x         y
-[1,] 20.900849 22.269930
-[2,] 21.319102 22.223864
-[3,] 17.937159 18.258445
-[4,] 15.257954 20.421463
-
-21.319102-10.97643
-22.223864-10.78619
-x        y
-10.97643 10.78619
-
-x  9.92442 10.34267 6.960729 4.281525 8.374418 7.134973  7.491791 7.116012 8.227562  8.401136 10.615756 10.996427
-y 11.48374 11.43768 7.472257 9.635275 9.352603 5.888514 10.835
-
-library(mvnfast)
-dnorm()
-?dnorm
+graph() ## 그래프로 확인
 
 
 
+  

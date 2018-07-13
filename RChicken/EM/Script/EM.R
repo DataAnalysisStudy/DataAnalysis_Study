c28ae5a5ac598bbccd865f62afebe01b063e4497
kiminch3000 <kiminch3000@naver.com>
2018-07-13 16:43:15 +0900

    Update EM.R

--- RChicken/EM/Script/EM.R
+++ RChicken/EM/Script/EM.R
@@ -1,5 +1,6 @@
 rm(list = ls())
 library(plotly)
+library(plyr)
 
 ####### 데이터 셋 구축
 
@@ -22,26 +23,56 @@ data <- data.frame(x = x, y = y)
 
 ####### 원 중심과 sigma 1, 2 등고선 그리기 
 
-graph <- function(){
-  plot_ly(data, x = ~x, y = ~y, color = ~class, type = "scatter") %>%
-    add_trace(x = ~x, y = ~y, color = ~class, data = parameter, type = "scatter") %>%
-    layout(shapes = list(
-      list(xref = 'x', yref = 'y', 
-           x0 = parameter$x[1] - parameter$sigma[1], 
-           x1 = parameter$x[1] + parameter$sigma[1], 
-           y0 = parameter$y[1] - parameter$sigma[1],
-           y1 = parameter$y[1] + parameter$sigma[1], 
-           type = "circle", opacity = 0.2,
-           fillcolor = 'rgb(50, 20, 90)', line = list(color = 'rgb(50, 20, 90)')), 
-      list(xref = 'x', yref = 'y', 
-           x0 = parameter$x[2] - parameter$sigma[2], 
-           x1 = parameter$x[2] + parameter$sigma[2], 
-           y0 = parameter$y[2] - parameter$sigma[2],
-           y1 = parameter$y[2] + parameter$sigma[2], 
-           type = "circle", opacity = 0.2,
-           fillcolor = 'rgb(90, 200, 75)', line = list(color = 'rgb(90, 200, 75)'))
-    )
-    )
+graph <- function(dimension){
+  if(dimension == 2){
+    plot_ly(data, x = ~x, y = ~y, color = ~class, type = "scatter") %>%
+      add_trace(x = ~x, y = ~y, color = ~class, data = parameter, type = "scatter") %>%
+      layout(shapes = list(
+        list(xref = 'x', yref = 'y', 
+             x0 = parameter$x[1] - parameter$sigma[1], 
+             x1 = parameter$x[1] + parameter$sigma[1], 
+             y0 = parameter$y[1] - parameter$sigma[1],
+             y1 = parameter$y[1] + parameter$sigma[1], 
+             type = "circle", opacity = 0.2,
+             fillcolor = 'rgb(50, 20, 90)', line = list(color = 'rgb(50, 20, 90)')), 
+        list(xref = 'x', yref = 'y', 
+             x0 = parameter$x[2] - parameter$sigma[2], 
+             x1 = parameter$x[2] + parameter$sigma[2], 
+             y0 = parameter$y[2] - parameter$sigma[2],
+             y1 = parameter$y[2] + parameter$sigma[2], 
+             type = "circle", opacity = 0.2,
+             fillcolor = 'rgb(90, 200, 75)', line = list(color = 'rgb(90, 200, 75)'))
+      )
+      )
+  }else if(dimension == 3){
+    plot_ly(data, x = ~x, y = ~y, color = ~class, type = "scatter") %>%
+      add_trace(x = ~x, y = ~y, color = ~class, data = parameter, type = "scatter") %>%
+      layout(shapes = list(
+        list(xref = 'x', yref = 'y', 
+             x0 = parameter$x[1] - parameter$sigma[1], 
+             x1 = parameter$x[1] + parameter$sigma[1], 
+             y0 = parameter$y[1] - parameter$sigma[1],
+             y1 = parameter$y[1] + parameter$sigma[1], 
+             type = "circle", opacity = 0.2,
+             fillcolor = 'rgb(50, 20, 90)', line = list(color = 'rgb(50, 20, 90)')), 
+        list(xref = 'x', yref = 'y', 
+             x0 = parameter$x[2] - parameter$sigma[2], 
+             x1 = parameter$x[2] + parameter$sigma[2], 
+             y0 = parameter$y[2] - parameter$sigma[2],
+             y1 = parameter$y[2] + parameter$sigma[2], 
+             type = "circle", opacity = 0.2,
+             fillcolor = 'rgb(90, 200, 75)', line = list(color = 'rgb(90, 200, 75)')),
+        list(xref = 'x', yref = 'y', 
+             x0 = parameter$x[3] - parameter$sigma[3], 
+             x1 = parameter$x[3] + parameter$sigma[3], 
+             y0 = parameter$y[3] - parameter$sigma[3],
+             y1 = parameter$y[3] + parameter$sigma[3], 
+             type = "circle", opacity = 0.2,
+             fillcolor = 'rgb(140, 130, 20)', line = list(color = 'rgb(140, 130, 20)'))
+      )
+      )
+  }
+ 
 }
 
 
@@ -77,47 +108,99 @@ parameter <- rbind(parameter, mu)
 parameter <- cbind(parameter, sigma)
 parameter <- cbind(parameter, tou)
 parameter <- cbind(parameter, class = paste("center", factor(1:dimension), sep = "_"))
+rownames(parameter) <- 1:dimension
 
+################# EM 알고리즘 
 
-################# Expectation Step
-
-#### 초기 파라미터 설정한대로 데이터 class 지정 
-for(i in 1:dimension){
+EM <- function(data, dimension = dimension){
   
-  if(i == 1){
+  ############# Expectation Step
+  count <- 0
+  theta <- c()
+
+  #### likelihood 최대화 파라미터 구하기 
+  fxCal <- function(data){
+   
+    count <<- count + 1 
+
+    ## 첫 시작이면 공통 sigma, 두번째부터 class 별로 나누어서 sigma 넣기 
+    if(count == 1){
+      
+      covTemp <- rep(list(cov(data)), dimension)
+      covInv <- lapply(covTemp, solve)
+      
+    }else{
+      
+      ############# Max Step의 변수 변환 부분 
+      parameter$tou <<- apply(expectation$tou, 2, sum)/sum(expectation$tou) ## tou 변경
+      parameter[, 1:dimension] <<- aggregate(. ~ class, data = data, mean)[, -1] ## mu 변경
+      
+      dataTemp <- data
+      covTemp <- list()
+      for(j in 1:dimension){
+        
+        index <- dataTemp$class == j
+        x_mu <- as.matrix(dataTemp[index, 1:dimension] - as.matrix(parameter[j, 1:dimension]))
+        covTemp[[j]] <- t(as.matrix(expectation$tou[index, ]) * x_mu) %*% x_mu / sum(expectation$tou[index, ])
+        
+      }
+
+      covInv <- lapply(covTemp, solve)
+      
+      parameter[, "sigma"] <<- unlist(lapply(covTemp, function(x){
+        mean(x * diag(1, dimension))
+      }))
+      
+      rm(j)
+      
+    }
+
+    for(i in 1:dimension){
+      
+      #### 초기 파라미터 설정한대로 데이터 class 지정 
+      if(i == 1){ 
+        
+        fx <- data.frame(apply(data[, 1:dimension], 1, function(rowData){
+          x_mu <- as.numeric(rowData - parameter[i, 1:dimension])
+          exp(-1/2 * t(x_mu) %*% covInv[[i]] %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp[[i]])) ## i 정규분포에 속할 확률
+        }))
+        
+      }else{
+        
+        fx <- cbind(fx, apply(data[, 1:dimension], 1, function(rowData){
+          x_mu <- as.numeric(rowData - parameter[i, 1:dimension])
+          exp(-1/2 * t(x_mu) %*% covInv[[i]] %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp[[i]])) ## i 정규분포에 속할 확률
+        }))
+        
+      }
+      
+      if(i == dimension){
+       
+        colnames(fx) <- colnames(mu)
+        tou <- data.frame(t(apply(fx, 1, function(rowData){
+          rowData/sum(rowData)
+          })))
+        
+        }
+    }
     
-    covTemp <- cov(data)
-    covInv <- solve(covTemp)
-    
-    fx <- data.frame(apply(data, 1, function(x){
-      x_mu <- as.numeric(x - parameter[i, 1:dimension])
-      exp(-1/2 * t(x_mu) %*% covInv %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp)) ## i 정규분포에 속할 확률
-    }))
-    
-  }else{
-    
-    fx <- cbind(fx, apply(data, 1, function(x){
-      x_mu <- as.numeric(x - parameter[i, 1:dimension])
-      exp(-1/2 * t(x_mu) %*% covInv %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp)) ## i 정규분포에 속할 확률
-    }))
+    return(list(fx = fx, tou = tou))
     
   }
-  
-  if(i == dimension){
-    
-    rm(covTemp)
-    rm(covInv)
-    rm(i)
-    colnames(fx) <- colnames(mu)
-    
-  }
+
+  ############# Max Step
+  expectation <- fxCal(data)
+  data$class <- as.factor(apply(expectation$fx, 1, which.max)) ## likelihood가 가장 높은 클러스터 배정
+  theta <- c(theta, sum(expectation$tou * expectation$fx))
+  graph(dimension)
+  parameter
   
 }
 
-data$class <- as.factor(apply(fx, 1, which.max)) ## 초기값을 이용하여 likelihood가 가장 높은 클러스터에 배정
+cbind(head(expectation$tou), head(expectation$fx), head(expectation$tou * expectation$fx))
+
+
 
 graph() ## 그래프로 확인
 
 
-
-  

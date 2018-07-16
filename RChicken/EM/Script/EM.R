c60568bd086b8b46787a7cf8ebf66569cb5b2e32
kiminch3000 <kiminch3000@naver.com>
2018-07-16 19:56:53 +0900

    Update EM.R

--- RChicken/EM/Script/EM.R
+++ RChicken/EM/Script/EM.R
@@ -1,15 +1,16 @@
 rm(list = ls())
 library(plotly)
 library(plyr)
+library(mvtnorm)
 
 ####### 데이터 셋 구축
 
-x1 <- rnorm(50, 10, 2)
-x2 <- rnorm(50, 20, 1.5)
-x3 <- rnorm(50, 30, 2)
-y1 <- rnorm(50, 20, 2)
-y2 <- rnorm(50, 10, 2)
-y3 <- rnorm(50, 30, 2)
+x1 <- rnorm(50, 10, 1)
+x2 <- rnorm(50, 20, 1)
+x3 <- rnorm(50, 30, 1)
+y1 <- rnorm(50, 20, 1)
+y2 <- rnorm(50, 10, 1)
+y3 <- rnorm(50, 30, 1)
 
 x <- c(x1, x2)
 y <- c(y1, y2)
@@ -19,7 +20,7 @@ y <- c(y1, y2)
 
 data <- data.frame(x = x, y = y)
 
-
+# plot_ly(data, x = ~x, y = ~y)
 
 ####### 원 중심과 sigma 1, 2 등고선 그리기 
 
@@ -72,7 +73,7 @@ graph <- function(dimension){
       )
       )
   }
- 
+  
 }
 
 
@@ -93,10 +94,12 @@ mu <- apply(data, 2, function(x){
 mu <- mu[-c(1, nrow(mu)), ]
 
 #### sigma는 각각의 차원의 0.841분위서에서 0.5분위수(1sigma)를 뺀 값 
+
 sigma <- apply(data, 2, function(x){
   diff(quantile(x, c(0.5, 0.841)))
 }
 )
+sigma <- rep(mean(sigma), dimension)
 
 #### 각각의 차원에 추출될 확률은 균등분포로 설정
 tou <- rep(1/dimension, dimension)
@@ -109,6 +112,9 @@ parameter <- cbind(parameter, sigma)
 parameter <- cbind(parameter, tou)
 parameter <- cbind(parameter, class = paste("center", factor(1:dimension), sep = "_"))
 rownames(parameter) <- 1:dimension
+# 
+# parameter$x <- c(15, 15)
+# parameter$y <- c(10, 20)
 
 ################# EM 알고리즘 
 
@@ -117,35 +123,44 @@ EM <- function(data, dimension = dimension){
   ############# Expectation Step
   count <- 0
   theta <- c()
-
+  
   #### likelihood 최대화 파라미터 구하기 
   fxCal <- function(data){
-   
+    
     count <<- count + 1 
-
+    
     ## 첫 시작이면 공통 sigma, 두번째부터 class 별로 나누어서 sigma 넣기 
     if(count == 1){
+  
+      covTemp <- apply(parameter, 1, function(x){
+        
+        data.frame(cov(mapply(rnorm,
+                        rep(100, dimension),
+                        mean = as.numeric(x[1:dimension]),
+                        sd = as.numeric(rep(x[dimension + 1], dimension)))))
+        
+      })
       
-      covTemp <- rep(list(cov(data)), dimension)
-      covInv <- lapply(covTemp, solve)
+      covTemp <- lapply(covTemp, as.matrix)
       
     }else{
       
-      ############# Max Step의 변수 변환 부분 
-      parameter$tou <<- apply(expectation$tou, 2, sum)/sum(expectation$tou) ## tou 변경
+      ############# Max Step의 변수 변환 부분
       parameter[, 1:dimension] <<- aggregate(. ~ class, data = data, mean)[, -1] ## mu 변경
       
-      dataTemp <- data
       covTemp <- list()
       for(j in 1:dimension){
         
-        index <- dataTemp$class == j
-        x_mu <- as.matrix(dataTemp[index, 1:dimension] - as.matrix(parameter[j, 1:dimension]))
-        covTemp[[j]] <- t(as.matrix(expectation$tou[index, ]) * x_mu) %*% x_mu / sum(expectation$tou[index, ])
+        index <- data$class == j
+        parameter[j, "tou"] <<- sum(expectation$tou[index, j])/sum(expectation$tou[, j]) ## tou
+        x_mu <-  t(apply(data[index, 1:dimension], 1, function(rowData){
+          as.matrix(rowData - parameter[j, 1:dimension])
+        })) ## sigma
+        covTemp[[j]] <- (t(expectation$tou[index, j] * x_mu) %*% x_mu)/sum(expectation$tou[index, ])
         
       }
-
-      covInv <- lapply(covTemp, solve)
+      
+      parameter$tou <<- parameter$tou/sum(parameter$tou) ## tou 정규화 
       
       parameter[, "sigma"] <<- unlist(lapply(covTemp, function(x){
         mean(x * diag(1, dimension))
@@ -154,41 +169,47 @@ EM <- function(data, dimension = dimension){
       rm(j)
       
     }
-
+    
     for(i in 1:dimension){
       
       #### 초기 파라미터 설정한대로 데이터 class 지정 
       if(i == 1){ 
         
         fx <- data.frame(apply(data[, 1:dimension], 1, function(rowData){
-          x_mu <- as.numeric(rowData - parameter[i, 1:dimension])
-          exp(-1/2 * t(x_mu) %*% covInv[[i]] %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp[[i]])) ## i 정규분포에 속할 확률
+          
+          dmvnorm(as.numeric(rowData), 
+                  mean = as.numeric(parameter[i, 1:dimension]), 
+                  sigma = covTemp[[i]])
+          
         }))
         
       }else{
         
         fx <- cbind(fx, apply(data[, 1:dimension], 1, function(rowData){
-          x_mu <- as.numeric(rowData - parameter[i, 1:dimension])
-          exp(-1/2 * t(x_mu) %*% covInv[[i]] %*% x_mu) / sqrt((2 * pi)^dimension * det(covTemp[[i]])) ## i 정규분포에 속할 확률
+          
+          dmvnorm(as.numeric(rowData), 
+                  mean = as.numeric(parameter[i, 1:dimension]), 
+                  sigma = covTemp[[i]]) ## i 정규분포에 속할 확률
+          
         }))
         
       }
       
       if(i == dimension){
-       
+        
         colnames(fx) <- colnames(mu)
         
         tou <- data.frame(t(apply(t(t(fx) * parameter$tou), 1, function(rowData){
           rowData/sum(rowData)
-          })))
+        })))
         
-        }
+      }
     }
     
     return(list(fx = fx, tou = tou))
     
   }
-
+  
   ############# Max Step
   expectation <- fxCal(data)
   data$class <- as.factor(apply(expectation$fx, 1, which.max)) ## likelihood가 가장 높은 클러스터 배정
